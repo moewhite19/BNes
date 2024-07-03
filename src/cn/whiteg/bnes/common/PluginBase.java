4165d063fc27efb9fbea5b0d05e5f8fe9183428f
MoeWhite19 <MoeWhite19@163.com>
2024-07-03 17:05:33 +0800

    简化PluginBase

--- src/cn/whiteg/bnes/common/PluginBase.java
+++ src/cn/whiteg/bnes/common/PluginBase.java
@@ -5,52 +5,31 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Method;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class PluginBase extends JavaPlugin {
-    private final Map<String, Listener> listenerMap = new HashMap<>();
+    private final List<Listener> listenerList = new LinkedList<>();
 
     public void regListener(Listener listener) {
-        regListener(listener.getClass().getName(),listener);
-
+        listenerList.add(listener);
+        Bukkit.getPluginManager().registerEvents(listener,this);
     }
 
     public void regListener(String key,Listener listener) {
-//        getLogger().info("注册事件:" + key);
-        listenerMap.put(key,listener);
+        getLogger().info("注册事件:" + key);
         Bukkit.getPluginManager().registerEvents(listener,this);
-
     }
 
     public void unregListener() {
         try{
-            for (Map.Entry<String, Listener> entry : listenerMap.entrySet()) {
-                unregListener(entry.getValue());
+            for (Listener listener : listenerList) {
+                unregListener(listener);
             }
-            listenerMap.clear();
+            listenerList.clear();
         }catch (ConcurrentModificationException ignored){
         }catch (Exception e){
             e.printStackTrace();
         }
-    }
-
-
-    /**
-     * 卸载事件
-     *
-     * @param Key "卸载"
-     */
-    public boolean unregListener(String Key) {
-        Listener listenr = listenerMap.remove(Key);
-        if (listenr == null){
-            return false;
-        }
-        unregListener(listenr);
-        return true;
     }
 
     /**
@@ -59,29 +38,11 @@ public class PluginBase extends JavaPlugin {
      * @param listener 事件对象
      */
     public void unregListener(Listener listener) {
-        //注销事件
-        HandlerList.unregisterAll(listener);
-        if (listenerMap.remove(listener.getClass().getName()) == null){
-            //如果按照类名没搜索到对象则遍历
-            Iterator<Map.Entry<String, Listener>> it = listenerMap.entrySet().iterator();
-            while (it.hasNext()) {
-                if (it.next() == listener){
-                    it.remove();
-                    break;
-                }
-            }
-        }
-        //调用类中的unreg()方法
-        try{
-            Class listenerClass = listener.getClass();
-            Method unreg = listenerClass.getDeclaredMethod("unreg");
-            unreg.setAccessible(true);
-            unreg.invoke(listener);
-        }catch (Exception ignored){
-        }
+        if (listenerList.remove(listener)) HandlerList.unregisterAll(listener);
+
     }
 
-    public Map<String, Listener> getListenerMap() {
-        return listenerMap;
+    public List<Listener> getListenerList() {
+        return listenerList;
     }
 }
